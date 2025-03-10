72f6f95b9a852ebfd68ba888312418c05a6a7d91
alejandro naistat <alenaist10@gmail.com>
2025-03-10 19:22:49 -0300

    added analytics

--- peron-site/src/app/page.js
+++ peron-site/src/app/page.js
@@ -2,11 +2,13 @@
 
 import styles from "./page.module.css";
 import ScrollingBiography from "./components/ScrollingBiography";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <ScrollingBiography />
+      <Analytics />
     </div>
   );
 }
